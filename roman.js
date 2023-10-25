d26150d6b376166335fe458d7ab495cc6f00d136
soncreux <98056199+soncreux@users.noreply.github.com>
2023-10-25 14:08:53 +0300

    Add files via upload

---
+++ roman.js
@@ -0,0 +1,15 @@
+function romanToInt(s) {
+    const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let res = 0;
+    s.split('').forEach((num, i) => {
+        if (map[num] < map[s[i + 1]]) {
+            res -= map[num];
+        }
+        else {
+            res += map[num];
+        }
+    });
+    console.log(res);
+}
+
+romanToInt('VXL');
